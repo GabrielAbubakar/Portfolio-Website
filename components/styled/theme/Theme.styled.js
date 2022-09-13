bc41a9714434741780c7ce98147322bb9ddb8512
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-09-13 19:28:33 +0100

    added some custom themes

--- components/styled/theme/Theme.styled.js
+++ components/styled/theme/Theme.styled.js
@@ -42,3 +42,59 @@ export const light = {
         buttonStroke: '#ccc'
     }
 }
+
+// export const lightnew = {
+//     name: "light-theme",
+//     colors: {
+//         background: '#f3f5e8',
+//         bodyText: '#2c2b2b',
+//         primary: '#675C35',
+//         secondary: "#B3A577",
+//         headerText: '#504f4f',
+//         headerTextHighlight1: '#181818',
+//         subtext: '#8f876d',
+//         footerLinks: 'grey',
+//         footerLinksHover: '#222',
+
+
+//         thumbscrollThumb: "#777",
+//         thumbscrollHover: "#555",
+//         thumbscrollTrack: "#aaa",
+
+//         buttonBackground: '#181818',
+//         buttonStroke: '#ccc'
+//     }
+// }
+
+export const darkmax = {
+    name: "dark-theme",
+    colors: {
+        background: "#16181D",
+        headerText: "#E8E8FD",
+        primary: "#5686F5",
+        text: 'c0c9ca'
+    },
+};
+
+export const lightmax = {
+    name: "dark-theme",
+    colors: {
+        background: "#F9FAFB",
+        headerText: "#04052F",
+        primary: "#0e52f1",
+        text: '626b84'
+    },
+};
+
+
+
+// gradients
+// linear-gradient(90deg, rgb(184, 220, 255) 16.15%, rgb(107, 187, 255) 100%);
+// linear-gradient(90deg, rgb(231, 223, 255) 0%, rgb(250, 172, 168) 100%, rgb(255, 169, 165) 100%);
+// linear-gradient(90deg, rgb(255, 226, 210) 0%, rgb(255, 189, 205) 100%);
+// linear-gradient(90deg, rgb(218, 212, 236) 0%, rgb(218, 212, 236) 1%, rgb(243, 231, 233) 100%);
+// linear-gradient(90deg, rgb(231, 240, 253) 0%, rgb(172, 203, 238) 100%);
+//linear-gradient(90deg, rgb(243, 231, 233) 0%, rgb(227, 238, 255) 99%, rgb(227, 238, 255) 100%);
+
+// fonts
+// Inter Segou UI
